73c611194641a01f207194f9f0e930e3b74788b9
arnel <thesantos395@gmail.com>
2024-03-11 10:19:59 +0800

    answered 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+let initString=string;
+if (num===0){string=''}
+if (num<0){string="ERROR"}
+for(let i=0; i<(num-1); i++){    
+string = string + initString;
+}
+return string
 };
-
+repeatString('',4)
+//console.log(repeatString('hey',4))
 // Do not edit below this line
 module.exports = repeatString;
