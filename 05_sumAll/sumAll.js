54cb976f837347633edab519efef51bfa4abc759
arnel <thesantos395@gmail.com>
2024-06-01 21:47:48 +0800

    answewr a few

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
 
+ if(min<0 ||max<0 || !(typeof min === "number")||!(typeof max === "number")){
+    return "ERROR";
+ }
+    
+if (min>max){
+    first=max;
+     last=min;  
+}
+else{
+    first=min;
+    last=max;  
+}
+
+
+
+    let result = 0;
+
+for(let i=first; i<=last; i++){
+    result += i;
+}
+
+return result
+console.log(result);
+
+//
 };
+
+sumAll("a",4);
 
 // Do not edit below this line
 module.exports = sumAll;
